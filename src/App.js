1d74da693b9bccafc507e810f2154dbc19d9ad26
Marília Suzart <marilia.suzart@hotmart.com>
2019-08-16 08:04:23 -0300

    Add purecss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Routes from './Routes'
-import './App.scss';
+import './App.scss'
+import './css/pure-min.css'
+import './css/side-menu.css'
+
 
 function App() {
   return (
     <>
       <Routes />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
